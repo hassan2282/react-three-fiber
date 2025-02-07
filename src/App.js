db08bc36beed3013b5e6cbb049a66a04279c2546
سید حسن تقوی <taghavey.hassan@gmail.com>
2025-02-07 12:34:10 +0330

    final commit

--- src/App.js
+++ src/App.js
@@ -3,29 +3,64 @@ import { Icon } from 'react-icons-kit'
 import {eye} from 'react-icons-kit/feather/eye';
 import './App.css';
 import { Canvas, useFrame } from '@react-three/fiber';
+import { OrbitControls, useHelper } from '@react-three/drei';
+import { useControls } from 'leva';
+import { SpotLightHelper } from 'three';
+// import { FirstPersonControls } from '@react-three/drei';
 
 function App() {
 
+  function LightWithHelper () {
+    const light = useRef();
+    const {angle, penumbra} = useControls({
+      angle: Math.PI / 8,
+      penumbra: {
+        value: 0.0,
+        min: 0.0,
+        max: 1.0,
+        step: 0.1
+      }
+    })
+    
+    useHelper(light, SpotLightHelper, 'orange');
+    return (
+    <spotLight castShadow ref={light} penumbra={penumbra} angle={angle} intensity={80} color={0xffea00} position={[0,5,7]}/>
+    )
+  }
+  
   function AnimatedBox () {
+
+    
+    const {speed, color} = useControls({
+
+      color: 'red',
+      speed: {
+        value: 0.005,
+        min: 0.0,
+        max: 0.03,
+        step: 0.001
+      }
+    })
+    
     const boxRef = useRef()
     
     useFrame(()=>{
-      boxRef.current.rotation.x += 0.005;
-      boxRef.current.rotation.y += 0.005;
-      boxRef.current.rotation.z += 0.005;
+      boxRef.current.rotation.x += speed;
+      boxRef.current.rotation.y += speed;
+      boxRef.current.rotation.z += speed;
     })
 
     return (
-      <mesh ref={boxRef}>
+      <mesh ref={boxRef} castShadow>
         <boxGeometry args={[2,2,2]}/>
-        <meshStandardMaterial color="gold" />
+        <meshStandardMaterial color={color}/>
       </mesh>
     );
   }
 
   return (
-    <div className="flex flex-row h-screen w-screen bg-zinc-800 justify-center">
-      <Canvas className='w-32 h-32' camera={{position: [2,2,2]}}>
+    <div className={`flex flex-row h-screen w-screen bg-zinc-900 justify-center`}>
+      <Canvas shadows className='w-32 h-32' camera={{position: [4,2,1]}}>
 
 
 
@@ -36,9 +71,19 @@ function App() {
           {/* <meshBasicMaterial color="gold"/> */}
           {/* <meshPhongMaterial color="gold"/> */}
         {/* </mesh> */}
-
+          
+          {/* <FirstPersonControls movementSpeed={3}/> */}
+          <OrbitControls />
           <AnimatedBox />
-          <directionalLight position={[2,5,1]}/>
+          {/* <directionalLight position={[2,5,1]}/> */}
+          <ambientLight intensity={0.2} color={"gold"}/>
+          <directionalLight intensity={0.1} color={0xfcfcfc} position={[2,5,1]}/>
+          <LightWithHelper />
+          {/* <pointLight intensity={50} position={[2,5,1]}/> */}
+          <mesh rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
+            <planeGeometry args={[20,20]} />
+            <meshStandardMaterial />
+          </mesh>
       </Canvas>
     </div>
   );
